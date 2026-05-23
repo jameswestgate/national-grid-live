0f2cc65834c388ccdf9a8ed3ce475159020cc700
James Westgate <jameswestgate@icloud.com>
2026-05-23 21:38:50 +0100

    Initial Commit

---
+++ national grid live/national_grid_liveApp.swift
@@ -0,0 +1,17 @@
+//
+//  national_grid_liveApp.swift
+//  national grid live
+//
+//  Created by James Westgate on 23/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct national_grid_liveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
